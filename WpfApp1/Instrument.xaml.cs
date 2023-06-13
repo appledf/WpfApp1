9e0bf41e6506732c43011208eebfd94cbd1ef870
daifool@163.com <daifool@163.com>
2023-06-13 16:32:17 +0800

    添加项目文件。

---
+++ WpfApp1/Instrument.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Instrument.xaml 的交互逻辑
+    /// </summary>
+    public partial class Instrument : UserControl
+    {
+        public double Value
+        {
+            get { return (double)this.GetValue(ValueProperty); }
+            set { this.SetValue(ValueProperty, value); }
+        }
+        public static readonly DependencyProperty ValueProperty =
+                DependencyProperty.Register("Value", typeof(double), typeof(Instrument),
+                new PropertyMetadata(double.NaN, new PropertyChangedCallback(OnPropertyChanged)));
+        public static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as Instrument).Refresh();
+        }
+        public Instrument()
+        {
+            InitializeComponent();
+            this.SizeChanged += UCCycleProcessBar_SizeChanged;
+        }
+        private void UCCycleProcessBar_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double minSize = Math.Min(this.RenderSize.Width, this.RenderSize.Height);
+            this.backEllipse.Width = minSize;
+            this.backEllipse.Height = minSize;
+            //this.frontEllipse.Width = minSize - 50;
+            //this.frontEllipse.Height = minSize - 50;
+        }
+        private void Refresh()
+        {
+            //半径
+            double raduis = this.backEllipse.Width / 2;
+            this.mainCanvas.Children.Clear();
+            double min = 0, max = 100;
+            double scalAreaCount = 10;
+            double step = 270.0 / (max - min);
+            for (int i = 0; i < max - min; i++)
+            {
+                Line lineScale = new Line();
+                lineScale.X1 = raduis - (raduis - 13) * Math.Cos((i * step - 45) * Math.PI / 180);
+                lineScale.Y1 = raduis - (raduis - 13) * Math.Sin((i * step - 45) * Math.PI / 180);
+                lineScale.X2 = raduis - (raduis - 8) * Math.Cos((i * step - 45) * Math.PI / 180);
+                lineScale.Y2 = raduis - (raduis - 8) * Math.Sin((i * step - 45) * Math.PI / 180);
+
+                lineScale.Stroke = Brushes.White;
+                lineScale.StrokeThickness = 2;
+                this.mainCanvas.Children.Add(lineScale);
+            }
+            step = 270.0 / scalAreaCount;
+            int scaleText = (int)min;
+            for (int i = 0; i <= scalAreaCount; i++)
+            {
+                Line lineScale = new Line();
+                lineScale.X1 = raduis - (raduis - 20) * Math.Cos((i * step - 45) * Math.PI / 180);
+                lineScale.Y1 = raduis - (raduis - 20) * Math.Sin((i * step - 45) * Math.PI / 180);
+                lineScale.X2 = raduis - (raduis - 8) * Math.Cos((i * step - 45) * Math.PI / 180);
+                lineScale.Y2 = raduis - (raduis - 8) * Math.Sin((i * step - 45) * Math.PI / 180);
+
+                lineScale.Stroke = Brushes.White;
+                lineScale.StrokeThickness = 2;
+                this.mainCanvas.Children.Add(lineScale);
+
+                TextBlock textScale = new TextBlock();
+                textScale.Text = (scaleText + (max - min) / scalAreaCount * i).ToString();
+                textScale.Width = 34;
+                textScale.TextAlignment = TextAlignment.Center;
+                textScale.FontSize = 14;
+                textScale.Foreground = Brushes.White;
+                Canvas.SetLeft(textScale, raduis - (raduis - 36) * Math.Cos((i * step - 45) * Math.PI / 180) - 17);
+                Canvas.SetTop(textScale, raduis - (raduis - 36) * Math.Sin((i * step - 45) * Math.PI / 180) - 10);
+                this.mainCanvas.Children.Add(textScale);
+            }
+            //圆心装饰
+            string sData = "M{0} {1} A{0} {0} 0 1 1 {1} {2}";
+            sData = string.Format(sData, raduis / 2, raduis, raduis * 1.5);
+            var converter = TypeDescriptor.GetConverter(typeof(Geometry));
+            this.circle.Data = (Geometry)converter.ConvertFrom(sData);
+
+            //指针
+            step = 270.0 / (max - min);
+            //this.rtPointer.Angle = this.Value * step - 45;
+
+            DoubleAnimation da = new DoubleAnimation(this.Value * step - 45, new Duration(TimeSpan.FromMilliseconds(200)));
+            //this.rtPointer.BeginAnimation(ValueProperty, da);
+            //DoubleAnimation da = new DoubleAnimation((int)((this.Value - this.Minimum) * step) - 45, new Duration(TimeSpan.FromMilliseconds(200)));
+            this.rtPointer.BeginAnimation(RotateTransform.AngleProperty, da);
+
+            sData = "M{0} {1},{1} {2},{1} {3}";
+            sData = string.Format(sData, raduis * 0.3, raduis, raduis - 5, raduis + 5);
+            this.pointer.Data = (Geometry)converter.ConvertFrom(sData);
+        }
+    }
+}
---
+++ WpfApp1/UCCycleProcessBar.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// UCCycleProcessBar.xaml 的交互逻辑
+    /// </summary>
+    public partial class UCCycleProcessBar : UserControl
+    {
+        private int minValue = 0;
+        private int maxValue = 100;
+        public UCCycleProcessBar()
+        {
+            InitializeComponent();
+        }
+        private double _value;
+        private double _angle;
+        public double Value
+        {
+            get { return _value; }
+            set {
+                _value = value;
+                if (_value >= maxValue)
+                {
+                    _value = maxValue;
+                }
+                if (_value <= minValue)
+                {
+                    _value = minValue;
+                }
+                SetValue(_value); }
+        }
+        public double Step
+        { get; set; }
+        public double StartAngle
+        {
+            get { return _angle; }
+            set { 
+                _angle = value;
+                if (_angle > 0.0)
+                {
+                    RotateTransform rt = new RotateTransform();
+                    rt.Angle = StartAngle;
+                    mainCanvas.RenderTransformOrigin = new Point(0.5, 0.5);
+                    mainCanvas.RenderTransform = rt;
+                }
+            } 
+        }
+
+        private void btnReduce_Click(object sender, RoutedEventArgs e)
+        {
+            this.Value = this.Value - Step;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            this.Value = this.Value + Step;
+        }
+
+        /// <summary>
+        /// 设置百分百，输入小数，自动乘100
+        /// </summary>
+        /// <param name="percentValue"></param>
+        private void SetValue(double Value)
+        {
+            /*****************************************
+              方形矩阵边长为34，半长为17
+              环形半径为14，所以距离边框3个像素
+              环形描边3个像素
+            ******************************************/
+            double percentValue = Value / Convert.ToDouble(maxValue);
+            double angel = percentValue * 360; //角度
+
+            double radius = 140; //环形半径
+
+            //起始点
+            double leftStart = 170;
+            double topStart = 30;
+            //结束点
+            double endLeft = 0;
+            double endTop = 0;
+
+            //数字显示
+            lbValue.Content = (percentValue * 100).ToString("0") + "%";
+
+            /***********************************************
+            * 整个环形进度条使用Path来绘制，采用三角函数来计算
+            * 环形根据角度来分别绘制，以90度划分，方便计算比例
+            ***********************************************/
+
+            bool isLagreCircle = false; //是否优势弧，即大于180度的弧形
+
+            //小于90度
+            if (angel <= 90)
+            {
+                double ra = (90 - angel) * Math.PI / 180; //弧度
+                endLeft = leftStart + Math.Cos(ra) * radius; //余弦横坐标
+                endTop = topStart + radius - Math.Sin(ra) * radius; //正弦纵坐标
+
+            }
+            else if (angel <= 180)
+            {
+                double ra = (angel - 90) * Math.PI / 180; //弧度
+                endLeft = leftStart + Math.Cos(ra) * radius; //余弦横坐标
+                endTop = topStart + radius + Math.Sin(ra) * radius;//正弦纵坐标
+            }
+
+            else if (angel <= 270)
+            {
+                isLagreCircle = true; //优势弧
+                double ra = (angel - 180) * Math.PI / 180;
+                endLeft = leftStart - Math.Sin(ra) * radius;
+                endTop = topStart + radius + Math.Cos(ra) * radius;
+            }
+
+            else if (angel < 360)
+            {
+                isLagreCircle = true; //优势弧
+                double ra = (angel - 270) * Math.PI / 180;
+                endLeft = leftStart - Math.Cos(ra) * radius;
+                endTop = topStart + radius - Math.Sin(ra) * radius;
+            }
+            else
+            {
+                isLagreCircle = true; //优势弧
+                endLeft = leftStart - 0.001; //不与起点在同一点，避免重叠绘制出非环形
+                endTop = topStart;
+            }
+            //结尾圆
+            Canvas.SetLeft(Pointer, endLeft-20);
+            Canvas.SetTop(Pointer, endTop-20);
+
+            Point arcEndPt = new Point(endLeft, endTop); //结束点
+            Size arcSize = new Size(radius, radius);
+            SweepDirection direction = SweepDirection.Clockwise; //顺时针弧形
+            //弧形
+            ArcSegment arcsegment = new ArcSegment(arcEndPt, arcSize, 0, isLagreCircle, direction, true);
+
+            //形状集合
+            PathSegmentCollection pathsegmentCollection = new PathSegmentCollection();
+            pathsegmentCollection.Add(arcsegment);
+
+            //路径描述
+            PathFigure pathFigure = new PathFigure();
+            pathFigure.StartPoint = new Point(leftStart, topStart); //起始地址
+            pathFigure.Segments = pathsegmentCollection;
+
+            //路径描述集合
+            PathFigureCollection pathFigureCollection = new PathFigureCollection();
+            pathFigureCollection.Add(pathFigure);
+
+            //复杂形状
+            PathGeometry pathGeometry = new PathGeometry();
+            pathGeometry.Figures = pathFigureCollection;
+
+            //Data赋值
+            //myCycleProcessBar.Data = pathGeometry;
+            myCycleProcessBar1.Data = pathGeometry;
+            //达到100%则闭合整个
+            if (angel == 360)
+                myCycleProcessBar1.Data = Geometry.Parse(myCycleProcessBar1.Data.ToString() + " z");
+        }
+    }
+}
